26dcaf5bdc500b1f3dba8410270e63d575afec79
Justin <justinprivitera@gmail.com>
2020-10-16 12:36:03 -0700

    moving square!

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import sys
+# import sys
 
 # import OpenGL
 import OpenGL.GL as GL
@@ -9,6 +9,7 @@ import OpenGL.GLU as GLU
 
 width = 500
 height = 500
+x = 0
 
 def iterate():
 	GL.glViewport(0, 0, width, height)
@@ -18,21 +19,23 @@ def iterate():
 	GL.glMatrixMode (GL.GL_MODELVIEW)
 	GL.glLoadIdentity()
 
-def showScreen():
+def render():
+	global x
 	GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT) # Remove everything from screen (i.e. displays all white)
 	GL.glLoadIdentity() # Reset all graphic/shape's position
 	iterate()
-	GL.glColor3f(1.0, 0.0, 3.0) # Set the color to pink
-	square() # Draw a square using our function
+	GL.glColor3f(3.0, 0.5, 0.0) # Set the color
+	x += 0.01
+	square(x) # Draw a square using our function
 	GLUT.glutSwapBuffers()
 
-def square():
+def square(x):
 	# We have to declare the points in this sequence: bottom left, bottom right, top right, top left
 	GL.glBegin(GL.GL_QUADS) # Begin the sketch
-	GL.glVertex2f(100, 100) # Coordinates for the bottom left point
-	GL.glVertex2f(200, 100) # Coordinates for the bottom right point
-	GL.glVertex2f(200, 200) # Coordinates for the top right point
-	GL.glVertex2f(100, 200) # Coordinates for the top left point
+	GL.glVertex3f(100 + x, 100 + x, 0) # Coordinates for the bottom left point
+	GL.glVertex3f(200 + x, 100 + x, 0) # Coordinates for the bottom right point
+	GL.glVertex3f(200 + x, 200 + x, 0) # Coordinates for the top right point
+	GL.glVertex3f(100 + x, 200 + x, 0) # Coordinates for the top left point
 	GL.glEnd() # Mark the end of drawing
 
 GLUT.glutInit() # Initialize a glut instance which will allow us to customize our window
@@ -40,6 +43,6 @@ GLUT.glutInitDisplayMode(GLUT.GLUT_RGBA) # Set the display mode to be colored
 GLUT.glutInitWindowSize(width, height)   # Set the width and height of your window
 GLUT.glutInitWindowPosition(0, 0)   # Set the position at which this windows should appear
 wind = GLUT.glutCreateWindow("hello triangle") # Give your window a title
-GLUT.glutDisplayFunc(showScreen)  # Tell OpenGL to call the showScreen method continuously
-GLUT.glutIdleFunc(showScreen)   # Draw any graphics or shapes in the showScreen function at all times
+GLUT.glutDisplayFunc(render)  # Tell OpenGL to call the render method continuously
+GLUT.glutIdleFunc(render)   # Draw any graphics or shapes in the render function at all times
 GLUT.glutMainLoop()  # Keeps the window created above displaying/running in a loop
